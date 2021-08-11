f540003a7672d758bf2872f5e88cba466a2a1756
Marco Polo <marcopolocode@gmail.com>
2021-08-11 01:07:14 -0500

    scroll

---
+++ js/index.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.mostrando',{
+
+    duration: 3000,
+    origin
+
+});
+
+/**https://scrollrevealjs.org/guide/hello-world.html
+ * 
+ * https://www.youtube.com/watch?v=bs6WPzeCF7I
+ * 
+ */
