f84df61cb5c4808dcf44c953f67230d8d18712e2
Marco Polo <marcopolocode@gmail.com>
2021-08-12 14:06:02 -0500

    2:05pm

--- js/medidor.js
+++ js/medidor.js
@@ -1,4 +1,9 @@
-if(screen.width <= 1200)
+if(screen.width <= 1200 && screen.height<=1366)
 {
     document.getElementById('slider').innerHTML='<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTSYesVcK4tlJt-qJs3OBMO7--VGx7V16GUH97CWHUxr3EJiNI9pSonwBE2dAZYOw/embed?start=false&loop=false&delayms=3000" frameborder="0" width="640" height="389" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
+}
+
+else if(screen.width <= 300 && screen.height <= 640)
+{
+    document.getElementById('slider').innerHTML='<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTSYesVcK4tlJt-qJs3OBMO7--VGx7V16GUH97CWHUxr3EJiNI9pSonwBE2dAZYOw/embed?start=false&loop=false&delayms=3000" frameborder="0" width="350" height="389" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
 }
