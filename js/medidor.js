138d0cb53cc0cb67faeb57523f034a0d268324f8
Marco Polo <marcopolocode@gmail.com>
2021-08-14 01:50:11 -0500

    social network

--- js/medidor.js
+++ js/medidor.js
@@ -1,9 +1,16 @@
 if(screen.width <= 1200 && screen.height<=1366)
 {
     document.getElementById('slider').innerHTML='<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTSYesVcK4tlJt-qJs3OBMO7--VGx7V16GUH97CWHUxr3EJiNI9pSonwBE2dAZYOw/embed?start=false&loop=false&delayms=3000" frameborder="0" width="640" height="389" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
+alert("se agrego iframe 1");
+console.log("se agrego iframe 1");
+
 }
 
-else if(screen.width <= 300 && screen.height <= 640)
+ if(screen.width <= 400 && screen.height <= 700)
 {
     document.getElementById('slider').innerHTML='<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTSYesVcK4tlJt-qJs3OBMO7--VGx7V16GUH97CWHUxr3EJiNI9pSonwBE2dAZYOw/embed?start=false&loop=false&delayms=3000" frameborder="0" width="350" height="389" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
+    alert("se agrego iframe 2");
+    console.log("se agrego iframe 2");
+
+
 }
